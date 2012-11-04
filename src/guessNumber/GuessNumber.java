52c54e762839c58de9727b25c7d400fbf00335be
xianjing <zhuoxianjing@gmail.com>
2012-11-04 15:47:14 +0800

    validate user input and return nAnB

--- src/guessNumber/GuessNumber.java
+++ src/guessNumber/GuessNumber.java
@@ -2,16 +2,48 @@ package guessNumber;
 
 public class GuessNumber {
 
-    private String randomNumber;
+    private String serverNumber;
 
-    public GuessNumber(String randomNumber) {
-        this.randomNumber = randomNumber;
+    public GuessNumber(String serverNumber) {
+        this.serverNumber = serverNumber;
     }
 
     public String validate(String userInput) {
-        if(userInput.equals(randomNumber)){
-            return "4a0b";
-        }
-        return "3a0b";
+        return String.format("%da%db",
+                getNumberOfA(userInput, serverNumber),
+                getNumberOfB(userInput, serverNumber));
     }
+
+    private int getNumberOfB(String input, String originalNumber) {
+        int number = 0;
+        char[] inputChars = input.toCharArray();
+        for(int position = 0; position < 4; position++){
+            int index = originalNumber.indexOf(inputChars[position]);
+            if(isDigitCorrectButPositionNot(position, index)) {
+                number ++;
+            }
+        }
+        return number;
+    }
+
+    private int getNumberOfA(String input, String originalNumber) {
+        char[] inputChars = input.toCharArray();
+        int number = 0;
+        for(int position = 0; position < 4; position++){
+            int index = originalNumber.indexOf(inputChars[position]);
+            if(isDigitExactlyMatch(position, index)) {
+                number++;
+            }
+        }
+        return number;
+    }
+
+    private boolean isDigitCorrectButPositionNot(int i, int index) {
+        return index != -1 && index != i;
+    }
+
+    private boolean isDigitExactlyMatch(int i, int index) {
+        return index != -1 && index == i;
+    }
+
 }
--- test/GuessNumberTest.java
+++ test/GuessNumberTest.java
@@ -18,4 +18,40 @@ public class GuessNumberTest {
         String result = guessNumber.validate("1235");
         assertEquals("3a0b", result);
     }
+
+    @Test
+    public void should_return_2a0b_given_partial_digits_are_correct(){
+        GuessNumber guessNumber = new GuessNumber("1234");
+        String result = guessNumber.validate("1256");
+        assertEquals("2a0b", result);
+    }
+
+    @Test
+    public void should_return_2a1b_given_partial_digits_are_correct(){
+        GuessNumber guessNumber = new GuessNumber("1234");
+        String result = guessNumber.validate("1246");
+        assertEquals("2a1b", result);
+    }
+
+    @Test
+    public void should_return_2a2b_given_partial_digits_are_correct(){
+        GuessNumber guessNumber = new GuessNumber("1234");
+        String result = guessNumber.validate("1243");
+        assertEquals("2a2b", result);
+    }
+
+    @Test
+    public void should_return_0a2b_given_partial_digits_are_correct(){
+        GuessNumber guessNumber = new GuessNumber("1234");
+        String result = guessNumber.validate("6743");
+        assertEquals("0a2b", result);
+    }
+
+    @Test
+    public void should_return_0a0b_given_partial_digits_are_correct(){
+        GuessNumber guessNumber = new GuessNumber("1234");
+        String result = guessNumber.validate("5678");
+        assertEquals("0a0b", result);
+    }
+
 }
